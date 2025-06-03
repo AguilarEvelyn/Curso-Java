9786c25ae24c8f468e628140841f654f446d49df
Evelyn Aguilar <maryeliaguilar26@gmail.com>
2025-06-03 13:47:57 +0200

    Add git sample txt txt file

---
+++ demos/HolaMundo/HelloWorld.java
@@ -0,0 +1,12 @@
+package demos.HolaMundo;
+
+public class HelloWorld {
+
+   public static void main(String[] args) {
+        // comentario
+        String greetings = "Hola Mundo";
+        System.err.println("------ ");
+        System.out.println(greetings);
+
+    }
+}
