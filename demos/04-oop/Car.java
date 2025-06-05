6bc1e9d35a711fedac0b42741b7435fa45c53d46
Evelyn Aguilar <maryeliaguilar26@gmail.com>
2025-06-05 13:55:01 +0200

    add demo car

---
+++ demos/04-oop/Car.java
@@ -0,0 +1,22 @@
+public class Car {
+     String brand;
+     char type;
+    int numSeats;
+    int numDors;
+    String color;
+   boolean isAuto;
+   public Car(String brand, char type, int numSeats, int numDors, String color, boolean isAuto) {
+    this.brand = brand;
+    this.type = type;
+    this.numSeats = numSeats;
+    this.numDors = numDors;
+    this.color = color;
+    this.isAuto = isAuto;
+   }
+
+  
+
+    
+
+    
+}
---
+++ demos/04-oop/CarMain.java
@@ -0,0 +1,15 @@
+public class CarMain {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+
+        Car car1= new Car("Toyota", 'S', 5, 4, "Verde", true);
+        Car car2 = new Car("Lexus", "D", 6,4, "Negro", true);
+
+        
+        System.out.println();
+    }
+    
+}
