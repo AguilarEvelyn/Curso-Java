1390602182156f10833c0239f4e20e57b05735bb
Evelyn Aguilar <maryeliaguilar26@gmail.com>
2025-06-09 10:20:38 +0200

    add demo car

--- demos/04-oop/CarMain.java
+++ demos/04-oop/CarMain.java
@@ -6,10 +6,24 @@ public class CarMain {
     public static void main(String[] args) {
 
         Car car1= new Car("Toyota", 'S', 5, 4, "Verde", true);
-        Car car2 = new Car("Lexus", "D", 6,4, "Negro", true);
+        Car car2= new Car("Ford", 'C', 2, 2, "Negro", false);
 
         
-        System.out.println();
+       System.out.printf("Coche 1: Marca: %s | Tipo: %c | Asiento: %d | Puertas: %d | Color: %s | Automático: %b%n",
+        car1.brand,
+        car1.type,
+        car1.numSeats,
+        car1.numDors,
+        car1.color,
+        car1.isAuto);
+
+        System.out.printf("Coche 2: Marca: %s | Tipo: %c | Asiento: %d | Puertas: %d | Color: %s | Automático: %b%n",
+        car2.brand,
+        car2.type,
+        car2.numSeats,
+        car2.numDors,
+        car2.color,
+        car2.isAuto);
     }
     
 }
