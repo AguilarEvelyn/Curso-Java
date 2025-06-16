01d992aa927d6bfd1a3c474aab87b193e895cad0
Evelyn Aguilar <maryeliaguilar26@gmail.com>
2025-06-16 09:30:34 +0200

    add exercise Primes

---
+++ demos/12-iteration/Horror.java
@@ -0,0 +1,16 @@
+
+
+public class Horror {
+     public static void main(String[] args) {
+
+        for (int i = 0; i >= 0; i++) {
+            System.out.println("Horror " + i);
+
+            if (i >= 100) {
+                break;
+            }
+        }
+        
+    }
+    
+}
