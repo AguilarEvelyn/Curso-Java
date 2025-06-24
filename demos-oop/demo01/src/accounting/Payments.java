766aed1cbb46ce6affcef0d427bc75b73d7c090f
Evelyn Aguilar <maryeliaguilar26@gmail.com>
2025-06-24 10:09:14 +0200

    add class extension samples

---
+++ demos-oop/demo01/src/accounting/Payments.java
@@ -0,0 +1,25 @@
+
+
+// public enum Payments {
+//     BIZUM, 
+//     CARD,
+//     TRANSFER;
+// }
+
+public enum Payments {
+    BIZUM("Bizum"), 
+    CARD("Tarjeta de Crédito"),
+    TRANSFER("Transferencia");
+
+    private String name;
+    
+    private Payments(String name) {
+        this.name = name;
+    }
+    
+    @Override
+    public String toString() {
+        return name;
+    }
+
+}
